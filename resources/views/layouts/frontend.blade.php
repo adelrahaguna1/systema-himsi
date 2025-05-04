7a75b0a8ebaeb7f5643dab77dd0c85f5fcb20391
adelrahaguna1 <adeliarahaguna.01@gmail.com>
2025-05-04 21:12:43 +0700

    tambahkan link linkedin

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -124,7 +124,7 @@
                             </li>
                             <li class="mb-2 d-flex align-items-center">
                                 <img src="{{ asset('images/LinkedIn.png') }}" alt="LinkedIn" height="20" class="me-2">
-                                <a href="https://www.linkedin.com/company/himpunan-mahasiwa-sistem-informasi-jakarta/" target="_blank" class="text-white text-decoration-none">Himsi - Jakarta</a>
+                                <a href="https://www.linkedin.com/company/himpunan-mahasiwa-sistem-informasi-jakarta/" target="_blank" class="text-white text-decoration-none">Himsi Jakarta</a>
                             </li>
                         </ul>
                     </div>
